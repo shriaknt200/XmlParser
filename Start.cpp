b05d4d62ee20d0b41eb454f9d2a31cbe4f986280
sbadiger <shrikant200@gmail.com>
2018-10-20 09:23:49 +0530

    Start program file added

---
+++ Start.cpp
@@ -0,0 +1,11 @@
+/*
+ * Program to start the application and initiate the all constructor
+ */
+
+#include<iostream>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+   std::cout<<"\n  *** START OF APPLICATION ***\n";
+   return 0;
+}
